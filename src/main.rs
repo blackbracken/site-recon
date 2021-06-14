2e61ceeba09931a7e2e63437d3af4e57fc590d28
blackbracken <me@bracken.black>
2021-06-15 05:25:07 +0900

    separate logic as minimal libs

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,7 @@
-use reqwest::Result;
-
-#[derive(Debug)]
-struct ValorantNews {
-    title: String,
-    url: String,
-}
-
-const URL_ROOT: &str = "https://playvalorant.com";
-const URL_NEWS: &str = "https://playvalorant.com/ja-jp/news";
-
-async fn get_news() -> Result<Vec<ValorantNews>> {
-    let doc = reqwest::get(URL_NEWS).await?.text().await?;
-
-    let pat = easy_scraper::Pattern::new(
-        // classが自動生成によるものに見え, 頻繁に変更されないかweb-archiveで確認したが, 現状公開当初から変更されていない
-        r#"
-<a href="{{url}}">
-    <div class="NewsCard-module--copyContainer--1R7N-">
-        <p class="copy-02 NewsCard-module--description--3sFiD">{{title}}</p>
-    </div>
-</a>
-        "#,
-    )
-    .unwrap();
-
-    let result = pat.matches(&doc);
-
-    Ok(result
-        .into_iter()
-        .map(|map| ValorantNews {
-            title: map["title"].to_owned(),
-            url: URL_ROOT.to_owned() + map["url"].to_owned().as_ref(),
-        })
-        .collect())
-}
+use site_recon::get_news;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> reqwest::Result<()> {
     println!("Hello, world!");
 
     let res = get_news().await;
